4fcc5c915a3ec81e5689559d028994561dc243ef
LadaDzenis <lada.tranevskaya@mail.ru>
2024-07-27 19:50:56 +0300

    Add files via upload

---
+++ 1-4st_program.py
@@ -0,0 +1,11 @@
+# 1st program
+print(9**0.5*5)
+
+# 2st program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3st program
+print(1234%1000, 234//10, 5678%1000, 678//10, 23 + 67)
+
+# 4st program
+print(int(13.42), int(42.13 * 100), 4213 % 100, 13 == 13)
